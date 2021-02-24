5c8650229a078539a0cac15e743f161c04d6f8c8
pavana31 <pavana3101@gmail.com>
2021-02-23 21:42:11 -0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { times } from 'async';
 import App from './App';
-import Donut from './components/navbar/DonutChart';
+// import Donut from './components/navbar/DonutChart';
 
 test('renders learn react link', () => {
   render(<App />);
